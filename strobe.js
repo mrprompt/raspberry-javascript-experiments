e3fe8986f7efd93ac00efcafd32e7a189f3599a6
Thiago Paes <mrprompt@gmail.com>
2016-10-31 02:44:04 -0200

    Create strobe.js

---
+++ strobe.js
@@ -0,0 +1,9 @@
+var raspi = require('raspi-io');
+var five = require('johnny-five');
+var board = new five.Board({
+  io: new raspi()
+});
+
+board.on('ready', function() {
+  (new five.Led('P1-16')).strobe();
+});
